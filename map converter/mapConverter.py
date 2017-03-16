1dc5f975af40977744402ca181b7ff23d1ab2e80
Ryan Stillings <rystills@gmail.com>
2017-03-16 02:38:13 -0400

    mapConverter now supports directory search
    If no input file is specified, mapConverter will now attempt to convert
    all valid files in the current directory.

--- map converter/mapConverter.py
+++ map converter/mapConverter.py
@@ -4,49 +4,62 @@
 import os
 import sys
 
+supportedFileTypes = {"surf" : "terrain",
+                      "cror" : "cryore",
+                      "dugg" : "predug",
+                      "high" : "surface",
+                      "path" : "path",
+                      "fall" : "fall"
+                      }
+supportedFileExtensions = ["ol"]
 
+#get name, extension, and fileType of input file (fileType will be None if unspoorted)
 def getFileDetails(f):
+    global supportedFileTypes
+    global supportedFileExtensions
+    
     name, extension = os.path.splitext(f)
     normalizedName = name.lower()
-    fileType = None
-
-    if normalizedName.startswith("surf"):
-        fileType = "terrain"
-    elif normalizedName.startswith("cror"):
-        fileType = "cryore"
-    elif normalizedName.startswith("dugg"):
-        fileType = "predug"
-    elif normalizedName.startswith("high"):
-        fileType = "surface"
-    elif normalizedName.startswith("path"):
-        fileType = "path"
-    elif normalizedName.startswith("fall"):
-        fileType = "fall"
-    elif extension.lower().endswith(".ol"):
-        fileType = "ol"
+    
+    #section of name preceding the first _ tells us the file type if its a .map file, otherwise the extension tells us the file type (eg. ol file)
+    fileType = supportedFileTypes[normalizedName.split("_")[0]] if normalizedName.split("_")[0] in supportedFileTypes and extension == ".map" else \
+    extension[1:] if extension[1:] in supportedFileExtensions else None
 
     return {
         "name": name,
         "ext": extension,
         "type": fileType
     }
+    
+#attempt to convert all supported files in the current directory
+def convertDirFiles(dir = None):
+    if (dir == None):
+        dir = os.getcwd()
+    files = getFileList(dir)
+    for file in files:
+        convertFile(file)
+    
+#get list of files in specified directory (defaults to current directory if not specified)
+def getFileList(dir = None):
+    if (dir == None):
+        dir = os.getcwd()
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(dir):
+        f.extend(filenames)
+        return f
 
-
-def main():
-    try:
-        inputFile = sys.argv[1]
-        inputFilePath = os.path.abspath(inputFile)
-    except IndexError:
-        print("Please give the input file as the script's argument.")
-        return
-
+#attempt to convert input file to a .js data file that is able to be read in to the engine
+def convertFile(inputFile):
+    inputFilePath = os.path.abspath(inputFile)
     # First ensure that there is a file to convert in the working directory
     if not os.path.isfile(inputFilePath):
-        print("Error: file named: {0} not found -> exiting".format(inputFile))
+        print("Error: file named '{0}' not found".format(inputFile))
         return
-
     # Extract required details about the given file
     details = getFileDetails(inputFile)
+    if (details["type"] == None):
+        print("Error: file type of '{0}' not supported".format(inputFile))
+        return
     outputFile = "{0}.js".format(details["name"])
 
     # Delete conversion file if it already exists,
@@ -134,6 +147,15 @@ def main():
             file.write("{0};".format("\n".join(map(str, levelData))))
 
     print("Successfully wrote level data to file: {0}".format(outputFile))
+
+
+def main():
+    if (len(sys.argv) > 1):
+        inputFile = sys.argv[1]
+        convertFile(inputFile)
+    else:
+        print("No input file specified. converting all detected files in working dir")
+        convertDirFiles()
 
 if __name__ == "__main__":
     main()
