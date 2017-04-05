1cc5b2505284fa877716a10fb48c1448bff5203a
Ryan Stillings <rystills@gmail.com>
2017-04-05 04:49:01 -0400

    Applied fade-out effect to landslides

--- classes/Dynamite.js
+++ classes/Dynamite.js
@@ -26,7 +26,7 @@ Dynamite.prototype.update = function() {
 			this.effectTimer -= 1;
 			//update effect transparency to create a fade-out
 			this.drawSurface.clearRect(0,0,this.image.width,this.image.height);
-			this.drawSurface.globalAlpha = this.effectTimer/30;
+			this.drawSurface.globalAlpha = this.effectTimer/this.maxEffectTimer;
 			this.drawSurface.drawImage(this.image,0,0);
 			if (this.effectTimer == 0) {
 				this.die();
@@ -56,7 +56,7 @@ Dynamite.prototype.detonate = function() {
 		this.rect = new Rect(this.drawSurface.canvas.width,this.drawSurface.canvas.height);
 		this.setCenterX(centX);
 		this.setCenterY(centY);
-		this.effectTimer = 30;
+		this.effectTimer = this.maxEffectTimer;
 	}
 }
 
@@ -66,6 +66,7 @@ function Dynamite(space) {
 	this.type = "dynamite";
 	RygameObject.call(this,0,0,10,10,this.image,gameLayer);
 	this.space = space;
+	this.maxEffectTimer = 30;
 	this.igniteTimer = 200;
 	this.effectTimer = 0;
 	this.space.contains.push(this); //TODO: DECIDE IF ROCK RAIDERS SHOULD ALSO BE INCLUDED IN CONTAINS, SINCE THEY WILL BE BOUNCING AROUND SPACES A LOT ANYWAY
--- classes/LandSlide.js
+++ classes/LandSlide.js
@@ -1,6 +1,10 @@
 makeChild("LandSlide","RygameObject");
 LandSlide.prototype.update = function() {
 	this.life-=1;
+	//update effect transparency to create a fade-out
+	this.drawSurface.clearRect(0,0,this.image.width,this.image.height);
+	this.drawSurface.globalAlpha = this.life/this.maxLife;
+	this.drawSurface.drawImage(this.image,0,0);
 	if (this.life %10 == 0) { //deal damage to raiders in 5 intervals over lifespan (every 10 frames)
 		for (var i = 0; i < raiders.objectList.length; ++i) {
 			if (collisionRect(this,raiders.objectList[i])) {
@@ -17,6 +21,7 @@ function LandSlide(space) {
 	RygameObject.call(this,0,0,10,10,this.image,gameLayer);
 	this.setCenterX(space.centerX());
 	this.setCenterY(space.centerY());
-	this.life=81;
+	this.maxLife = 81;
+	this.life=this.maxLife;
 	this.damagePerInstance = 3;
 }
